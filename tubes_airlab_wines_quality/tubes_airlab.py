f5fc63d5f299bed51fbd064cde72a3260cab2e30
victorronaldopasaribu <victorronaldopasaribu25@gmail.com>
2019-11-27 23:43:16 +0700

    tubes_airlab_wines_quality_code

---
+++ tubes_airlab_wines_quality/tubes_airlab.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+from time import time
+from IPython.display import display
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import visuals as vs
+
+from IPython import get_ipython
+ipy = get_ipython()
+if ipy is not None:
+    ipy.run_line_magic('matplotlib','inline')
+
+data = pd.read_csv("D://winequality-red.csv", sep=';')
+display(data.head(n=5))
+
+data.isnull().any()
+
+data.info()
+
+n_wines = data.shape[0]
+quality_above_6 = data.loc[(data['quality'] > 6)]
+n_above_6 = quality_above_6.shape[0]
+
+quality_below_5 = data.loc[(data['quality'] < 5)]
+n_below_5 = quality_below_5.shape[0]
+
+quality_between_5 = data.loc[(data['quality'] >= 5) & (data['quality'] <= 6)]
+n_between_5 = quality_between_5.shape[0]
+
+greater_percent = n_above_6*100/n_wines
+
+print("Total number of wine data: {}".format(n_wines))
+print("Wines with rating 7 and above: {}".format(n_above_6))
+print("Wines with rating less than 5: {}".format(n_below_5))
+print("Wines with rating 5 and 6: {}".format(n_between_5))
+print("Percentage of wines with quality 7 and above: {:.2f}%".format(greater_percent))
+
+display(np.round(data.describe()))
+
+vs.distribution(data, "quality")
+
+pd.plotting.scatter_matrix(data, alpha = 0.3, figsize = (40,40), diagonal = 'kde');
+
+correlation = data.corr()
+# display(correlation)
+plt.figure(figsize=(14, 12))
+heatmap = sns.heatmap(correlation, annot=True, linewidths=0, vmin=-1, cmap="RdBu_r")
+
+#pH vs Fixed Acidity
+fixedAcidity_pH = data[['pH', 'fixed acidity']]
+gridA = sns.JointGrid(x="fixed acidity", y="pH", data=fixedAcidity_pH, size=6)
+gridA = gridA.plot_joint(sns.regplot, scatter_kws={"s": 10})
+gridA = gridA.plot_marginals(sns.distplot)
+
+#Fixed Acidity vs Citric Acid
+fixedAcidity_citricAcid = data[['citric acid', 'fixed acidity']]
+g = sns.JointGrid(x="fixed acidity", y="citric acid", data=fixedAcidity_citricAcid, size=6)
+g = g.plot_joint(sns.regplot, scatter_kws={"s": 10})
+g = g.plot_marginals(sns.distplot)
+
+#Quality vs Volatile Acidity
+fig, axs = plt.subplots(ncols=1,figsize=(10,6))
+sns.barplot(x='quality', y='volatile acidity', data=data, ax=axs)
+plt.title('quality VS volatile acidity')
+
+plt.tight_layout()
+plt.show()
+plt.gcf().clear()
+
+#Quality vs Alcohol
+fig, axs = plt.subplots(ncols=1,figsize=(10,6))
+sns.barplot(x='quality', y='alcohol', data=data, ax=axs)
+plt.title('quality VS alcohol')
+
+plt.tight_layout()
+plt.show()
+plt.gcf().clear()
+
+#detect outliers (masih error)
+for feature in data.keys():
+    Q1 = np.percentile(data[feature], q=25)
+    Q3 = np.percentile(data[feature], q=75)
+    interquartile_range = Q3 - Q1
+    step = 1.5 * interquartile_range
+    print("Data points considered outliers for the feature '{}':".format(feature))
+    display(data[~((data[feature] >= Q1 - step) & (data[feature] <= Q3 + step))])
+    outliers = []
+    good_data = data.drop(data.index[outliers]).reset_index(drop = True)
+    
